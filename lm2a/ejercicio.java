bdfef30e33acc9d45ad1768f7f8f93d4823f4f86
Noemí <nab@tragsa.es>
2023-09-28 09:29:02 +0200

    Añadida clase Main.

---
+++ lm2a/ejercicio.java
@@ -0,0 +1,9 @@
+package mn2a;
+public class ejercicio {
+public static void main (String[] args) {
+   BasicConfigurator.configure();
+   log.debug("esto es un logger de debugging");
+   log.info("esto es un logger de debugging");
+
+}
+}
