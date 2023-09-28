de65ad7f84f2c480a0962f0a00719d935eca02c4
Noemí <nab@tragsa.es>
2023-09-28 09:46:38 +0200

    Añadido comentario en la clase Main respecto a Logger

--- lm2a/ejercicio.java
+++ lm2a/ejercicio.java
@@ -4,6 +4,7 @@ public static void main (String[] args) {
    BasicConfigurator.configure();
    log.debug("esto es un logger de debugging");
    log.info("esto es un logger de debugging");
+   /* Logger requiere actualmente BasicConfigurator.configure() */
 
 }
 }
